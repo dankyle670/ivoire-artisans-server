8f14423fd803885c656b367cc8b277cdf6abd1b1
daniel komoe <daniel.komoe@epitech.eu>
2024-10-29 17:41:48 +0100

    [ADD] handling isfirst connection probleme

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -38,6 +38,7 @@ const UserSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     verified: { type: Boolean, default: false },
+    isFirstLogin: { type: Boolean, default: true },
     createdAt: { type: Date, default: Date.now },
 });
 
@@ -128,33 +129,33 @@ app.post('/api/login', async (req, res) => {
   const { email, password } = req.body;
 
   try {
-    // Vérifier si l'utilisateur existe
     const user = await User.findOne({ email });
     if (!user) {
       return res.status(400).json({ message: 'User not found' });
     }
 
-    // Vérifier le mot de passe
     const isPasswordValid = await bcrypt.compare(password, user.password);
     if (!isPasswordValid) {
       return res.status(400).json({ message: 'Invalid credentials' });
     }
 
-    // Vérifier si l'utilisateur est confirmé
     if (!user.verified) {
       return res.status(403).json({ message: 'Account not verified' });
     }
 
-    // Créer un token JWT si l'utilisateur est vérifié
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
-
-    // Réponse si l'utilisateur est confirmé
-    res.json({ message: 'Login successful', token, verified: true });
+    const isFirstLogin = user.isFirstLogin;
+    if (isFirstLogin) {
+      user.isFirstLogin = false;
+      await user.save();
+    }
+    res.json({ message: 'Login successful', token, verified: true, isFirstLogin });
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: 'Server error' });
   }
 });
+
 
 // end users route
 
