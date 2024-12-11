6e5da1dbaec6f0c568c28649be511ecd4dc1047c
daniel komoe <daniel.komoe@epitech.eu>
2024-12-11 08:08:44 +0100

    [ADD] adding /api/professionalProfile/:userId

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -447,6 +447,28 @@ app.post('/api/professionalProfile', async (req, res) => {
   }
 });
 
+app.get('/api/professionalProfile/:userId', async (req, res) => {
+  const { userId } = req.params;
+
+  try {
+    const profile = await ProfessionalProfile.findOne({ userId });
+    const user = await User.findById(userId);
+
+    if (!user || !profile) {
+      return res.status(404).json({ message: 'Profile or user not found' });
+    }
+
+    res.status(200).json({
+      profile,
+      artisanType: user.artisanType,
+    });
+  } catch (error) {
+    console.error('Error fetching profile:', error);
+    res.status(500).json({ message: 'Error fetching profile', error });
+  }
+});
+
+
 //handling login
 
 app.post('/api/login', async (req, res) => {
