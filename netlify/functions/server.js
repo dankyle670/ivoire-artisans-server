f0363a8011f0cff3631227969f920882da3b7679
daniel komoe <daniel.komoe@epitech.eu>
2024-12-11 08:37:52 +0100

    [ADD] adding /api/professionalProfile/:userId

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -451,7 +451,13 @@ app.get('/api/professionalProfile/:userId', async (req, res) => {
   const { userId } = req.params;
 
   try {
-    const objectId = mongoose.Types.ObjectId(userId); // Convert to ObjectId
+    // Validate userId
+    if (!mongoose.Types.ObjectId.isValid(userId)) {
+      return res.status(400).json({ message: 'Invalid user ID format' });
+    }
+
+    const objectId = new mongoose.Types.ObjectId(userId);
+
     const profile = await ProfessionalProfile.findOne({ userId: objectId });
     const user = await User.findById(objectId);
 
@@ -464,15 +470,13 @@ app.get('/api/professionalProfile/:userId', async (req, res) => {
       artisanType: user.artisanType,
     });
   } catch (error) {
-    console.error('Error fetching profile:', error); // Log the actual error
+    console.error('Error fetching profile:', error);
     res.status(500).json({
       message: 'Error fetching profile',
-      error: error.message || error, // Return detailed error information
+      error: error.message || error,
     });
   }
 });
-
-
 
 //handling login
 
